0a33172479dea96db6a564c7330ebb3094bda898
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-01 10:35:24 -0300

    change: implemented useCallback logic for userData updates

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,12 +1,44 @@
-import { createContext, useState } from 'react';
+import axios from 'axios';
+import { createContext, useCallback, useEffect, useState } from 'react';
 
 const UserContext = createContext({});
+
+const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today';
 
 export function UserProvider({ children }) {
 	const [userData, setUserData] = useState({});
 
+	console.log(userData);
+
+	const updateTodayData = useCallback((todayData) => {
+		setUserData((prevUserData) => {
+			return {
+				...prevUserData,
+				today: todayData,
+			};
+		});
+	}, []);
+
+	const fetchTodayData = useCallback(() => {
+		if (userData.token) {
+			axios
+				.get(URL, {
+					headers: { Authorization: `Bearer ${userData.token}` },
+				})
+				.then(({ data }) => updateTodayData(data))
+				.catch(console.error);
+		}
+	}, [userData.token, updateTodayData]);
+
+	useEffect(() => {
+		console.log('efeito rodou');
+		fetchTodayData();
+	}, [fetchTodayData]);
+
 	return (
-		<UserContext.Provider value={{ userData, setUserData }}>{children}</UserContext.Provider>
+		<UserContext.Provider value={{ userData, setUserData, fetchTodayData }}>
+			{children}
+		</UserContext.Provider>
 	);
 }
 
