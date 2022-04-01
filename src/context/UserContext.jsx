c8a00ae1bb68ae009d7614011b4e2ad1442ccff2
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-01 11:04:31 -0300

    refactor: removed unecessary callback declaration

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -8,25 +8,23 @@ const TODAY_URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/ha
 export function UserProvider({ children }) {
 	const [userData, setUserData] = useState({});
 
-	const updateTodayData = useCallback((todayData) => {
-		setUserData((prevUserData) => {
-			return {
-				...prevUserData,
-				today: todayData,
-			};
-		});
-	}, []);
-
 	const fetchTodayData = useCallback(() => {
 		if (userData.token) {
 			axios
 				.get(TODAY_URL, {
 					headers: { Authorization: `Bearer ${userData.token}` },
 				})
-				.then(({ data }) => updateTodayData(data))
+				.then(({ data }) => {
+					setUserData((prevUserData) => {
+						return {
+							...prevUserData,
+							today: data,
+						};
+					});
+				})
 				.catch(console.error);
 		}
-	}, [userData.token, updateTodayData]);
+	}, [userData.token]);
 
 	useEffect(fetchTodayData, [fetchTodayData]);
 
