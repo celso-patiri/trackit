a1a439a8664bfcad2bf6a1fde8f968ad4479574f
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-01 12:37:10 -0300

    change: image import to static svg

--- src/components/HabitCard/HabitsPage/HabitCard.jsx
+++ src/components/HabitCard/HabitsPage/HabitCard.jsx
@@ -25,7 +25,6 @@ export default function HabitCard({ name, days, id, announceDelete }) {
 					/>
 				))}
 			</Days>
-			{/* <Trashcan onClick={deleteHabit} src={trashcan} alt="delete" /> */}
 			<TrashCan deleteHabit={deleteHabit} />
 		</Card>
 	);
