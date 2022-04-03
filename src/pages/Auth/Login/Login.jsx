0681af62bc187889a52e0be2f95fa9f13957a328
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-03 18:47:41 -0300

    fix: login submit prevent default

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -64,6 +64,7 @@ export default function Login() {
 	}
 
 	function handleSubmit(e) {
+		e.preventDefault();
 		if (!userInfo.email || !userInfo.password) return;
 		setIsProcessingRequest(true);
 		axios
